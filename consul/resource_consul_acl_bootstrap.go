29044cadb577046fdb460f7f0a3f8b90568805e1
steven leadbeater <stevenleadbeater@live.co.uk>
2022-02-18 16:59:44 +0000

    Adding acl bootstrap and agent token

---
+++ consul/resource_consul_acl_bootstrap.go
@@ -0,0 +1,54 @@
+package consul
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"log"
+)
+
+func resourceConsulACLBootstrap() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceConsulACLBootstrapCreate,
+		Read:   resourceConsulACLBootstrapRead,
+		Delete: resourceConsulACLBootstrapDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"secret_id": {
+				Type:        schema.TypeString,
+				ForceNew:    true,
+				Computed:    true,
+				Optional:    true,
+				Description: "The secret id.",
+			},
+		},
+	}
+}
+
+func resourceConsulACLBootstrapCreate(d *schema.ResourceData, meta interface{}) error {
+	client, _, _ := getClient(d, meta)
+
+	log.Printf("[DEBUG] Creating ACL Bootstrap")
+
+	token, _, err := client.ACL().Bootstrap()
+	if err != nil {
+		return fmt.Errorf("error creating ACL token: %s", err)
+	}
+
+	log.Printf("[DEBUG] Created ACL token %q", token.AccessorID)
+
+	d.SetId(token.AccessorID)
+	d.Set("secret_id", token.SecretID)
+
+	return resourceConsulACLTokenRead(d, meta)
+}
+
+func resourceConsulACLBootstrapRead(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
+
+func resourceConsulACLBootstrapDelete(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
---
+++ consul/resource_consul_agent_token.go
@@ -0,0 +1,46 @@
+package consul
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceConsulAgentToken() *schema.Resource {
+	return &schema.Resource{
+		Create:             resourceConsulAgentTokenCreate,
+		Update:             resourceConsulAgentTokenCreate,
+		Read:               resourceConsulAgentTokenRead,
+		Delete:             resourceConsulAgentTokenDelete,
+		DeprecationMessage: "The consul_agent_Token resource will be deprecated and removed in a future version. More information: https://github.com/hashicorp/terraform-provider-consul/issues/46",
+
+		Schema: map[string]*schema.Schema{
+			"token": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceConsulAgentTokenCreate(d *schema.ResourceData, meta interface{}) error {
+	client, _, writeOptions := getClient(d, meta)
+	agent := client.Agent()
+
+	token := d.Get("token").(string)
+	_, err := agent.UpdateAgentACLToken(token, writeOptions)
+
+	if err != nil {
+		return fmt.Errorf("failed to update agent acl agent token: %v", err)
+	}
+
+	return nil
+}
+
+func resourceConsulAgentTokenRead(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
+
+func resourceConsulAgentTokenDelete(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
